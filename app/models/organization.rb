c012bce744c7de29e7e59fc86109375930e13183
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-03-14 12:16:38 +0300

    lastest

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,10 +1,8 @@
 class Organization < ActiveRecord::Base
   attr_accessible :name, :region
-<<<<<<< HEAD
   Region_List = [['Al Assiry'],  ['Al Bidda'], ['Al Dafna'], ['Al Hilal'], ['Al Mamoura'], ['Al Markhiya'], ['Al Mirqab Al Jadeed'], ['Al Nasr'], ['Al Sadd'], ['Al Waab'], ['Bin Mahmoud'], ['Madinat Khalifa'], ['Old Airport'], ['Onaiza'], ['Qutaifiya'], ['Ras Abu Aboud'], ['Rumeilah'], ['Wadi Al Sail'], ['West Bay']]
   validates :name, :region, :presence => true
   has_many :medics
-=======
   validates :name, :region, :presence => true
->>>>>>> df89ba0e5ab66756c483aed6128f876d62ccdbf1
+
 end
