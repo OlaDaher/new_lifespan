069cee444f866e7b32df83a88fba25c85adb3d02
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-03-16 03:38:22 +0300

    further password validations on models

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,10 +1,21 @@
 class Donor < ActiveRecord::Base
-  attr_accessible :active, :donor, :blood_type, :email, :first_name, :last_name, :password, :password_confirmation, :phone, :region, :social_network
+  attr_accessible  :donor, :active, :blood_type, :email, :first_name, :last_name, :password, :password_confirmation, :phone, :region, :social_network
+  validates :password,   :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => true,
+                       :on => :update
+  validates :password_confirmation, :presence => true                     
   has_secure_password
   before_save :format_phone
-  validates :email, :password, :password_confirmation, :first_name, :last_name, :phone, :blood_type, :region, :presence => true
+  validates :email, :active, :donor, :social_network, :first_name, :last_name, :phone, :blood_type, :region, :presence => true
   validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info|qa))$/i, :message => "is not a valid format"
   validates_format_of :phone, :with => /^(\+?\d{11}|\+?\d{3}?[-.]?\d{4}[-.]?\d{4})$/, :message => "should be 11 digits (country code needed) delimited with dashes only"
+  validates_format_of :password, :with => /^(?=.*\d)(?=.*([a-z]|[A-Z]))([\x20-\x7E]){8,40}$/, :message => "should be 8-40 characters & contain atleast 1 integer, 1 alphabet."
+  
   Region_List = [[''], ['Al Assiry'],  ['Al Bidda'], ['Al Dafna'], ['Al Hilal'], ['Al Mamoura'], ['Al Markhiya'], ['Al Nasr'], ['Al Sadd'], ['Al Waab'], ['Bin Mahmoud'], ['Madinat Khalifa'], ['Old Airport'], ['Onaiza'], ['Qutaifiya'], ['Ras Abu Aboud'], ['Rumeilah'], ['Wadi Al Sail'], ['West Bay']]
   Blood_List = [[''], ['A+'], ['A-'], ['B+'], ['B-'], ['AB+'], ['AB-'], ['O+'], ['O-']]
   Social_Networks = [[''], ['Facebook'], ['Twitter']]
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,7 @@ Donor.create(first_name:"Srin", last_name:"Chakrav", phone:"97466090897",
 	region:"West Bay", active:true, password:"lifespanproject1", 
 	password_confirmation:"lifespanproject1", donor:false) 
 
-  Donor.create(first_name:"Srin", last_name:"Chakrav", phone:"97466090897", 
-  	email:"oladaher@gmail.com", blood_type:"B+", social_network:"Facebook", 
-  	region:"West Bay", active:true, password:"default", 
-  	password_confirmation:"default", donor:true)
-
-  Donor.create(first_name:"Srin", last_name:"Chakrav", phone:"97466090897", 
-  	email:"aliya.hashim11@gmail.com", blood_type:"B+", social_network:"Facebook", 
-  	region:"West Bay", active:true, password:"default", 
-  	password_confirmation:"default", donor:true)
+  
   	
 Medic.create(first_name:"Srin", last_name:"Chakrav", organization_id:1,
 email:"aliya.hashim@hotmail.com",
