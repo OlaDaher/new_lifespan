64a4866529c2d33ced3a76d8826958b85ce63e07
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-03-18 15:37:18 +0300

    navigational changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   private
 
+  def current_ability
+  	@current_ability ||= Ability.new(current_donor)
+  end
+
+
+  def current_donor
+    @current_donor ||= Donor.find(session[:donor_id]) if session[:donor_id]
+  end
+
+  helper_method :current_donor
+
+  def current_medic
+    @current_medic ||= Medic.find(session[:medic_id]) if session[:medic_id]
+  end
+
+  helper_method :current_medic
 
 
   rescue_from CanCan::AccessDenied do |exception|
