37e84842fda2fe16a2b0edab08ffa9658dec807e
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-03-25 20:52:23 +0300

    variable medic login

--- app/controllers/medicsessions_controller.rb
+++ app/controllers/medicsessions_controller.rb
@@ -7,8 +7,12 @@ class MedicsessionsController < ApplicationController
     	medic  = Medic.find_by_email(params[:email])
  		  if medic && medic.authenticate(params[:password])
     		session[:medic_id] = medic.id
-    		redirect_to root_url, :notice => "Logged in!"
-  		else
+        if medic.admin == true
+    		redirect_to root_url, :notice => "Admin Logged in!"
+        else
+        redirect_to edit_medic_path(medic), :notice => "Please fill in your Personal Information!"
+  		  end
+      else
     		flash.now.alert = "Invalid email or password"
     		render "new"
   		end
--- app/models/medic.rb
+++ app/models/medic.rb
@@ -15,7 +15,7 @@ class Medic < ActiveRecord::Base
   has_secure_password
   before_save :format_phone
   belongs_to :organization
-  validates :email, :organization_id, :position, :presence => true
+  validates :email, :organization_id, :presence => true
   validates_format_of :phone, :with => /^(\+?\d{11}|\+?\d{3}?[-.]?\d{4}[-.]?\d{4})$/, :message => "should be 11 digits (country code needed) delimited with dashes only", :allow_blank => true
   validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info|qa))$/i, :message => "is not a valid format"
   
