f12221cdf43300dc76129a498d9300373ed82853
Aliya-cmuq <ahashim@qatar.cmu.edu>
2013-03-27 02:42:51 +0300

    email content (partial)

--- app/mailers/donor_mailer.rb
+++ app/mailers/donor_mailer.rb
@@ -1,11 +1,11 @@
 class DonorMailer < ActionMailer::Base
   default from: "lifespan.donation@gmail.com"
-  def new_donor_request(donor_par)
-    @donorEmail = donor_par.map{|o| o.email}  		
-      # @donors = Donor.all
-      # <form id="contactForm" method="post"
-      #     action="<?php echo $this->getUrl('donors/send') ?>">
-      # if () wont work with more than 500
+  def new_donor_request(donor_par, medic, org)
+    @donorEmail = donor_par.map{|o| o.email}
+    @medic_name = medic.proper_name
+    @medic_org = org.name
+    @medic_region = org.region
+    # @bloodtype = donor_par.map{|o| o.proper_name}
         mail(:to => "lifespan.donation@gmail.com", :bcc => @donorEmail,
         :subject => "Blood Donation Request")
   end
