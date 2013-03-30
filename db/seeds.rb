bc99020b9c7b65eb8ebd8b315c87cc218e6aff7d
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-03-30 22:31:41 +0300

    comprehensive seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,61 @@
 #  cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Donor.create(first_name:"Life", last_name:"Span", phone:"97466090897", 
+Donor.create(first_name:"Admin", last_name:"LifeSpan", phone:"97466090897", 
 	email:"lifespan.donation@gmail.com", blood_type:"B+", social_network:"Facebook", 
 	region:"West Bay", active:true, password:"lifespanproject1", 
 	password_confirmation:"lifespanproject1", admin:true) 
-  	
-Medic.create(first_name:"Srin", last_name:"Chakrav", organization_id:1,
-email:"aliya.hashim@hotmail.com",
-position:"West Bay", admin:false, password:"default",
-password_confirmation:"default")
 
-Organization.create(name:"", region:"West Bay")
+Donor.create(first_name:"Donor", last_name:"Labrat", phone:"974521346725", 
+	email:"book@gmail.com", blood_type:"A+", social_network:"Twitter", 
+	region:"Al Dafna", active:true, password:"book12", 
+	password_confirmation:"book12", admin:false) 
+
+Donor.create(first_name:"Volunteer", last_name:"Second", phone:"45213698572", 
+	email:"volume@gmail.com", blood_type:"O-", social_network:"Facebook", 
+	region:"Al Sadd", active:true, password:"volume12", 
+	password_confirmation:"volume12", admin:false) 
+
+
+Medic.create(first_name:"Aliya", last_name:"Hashim", organization_id:1,
+email:"aliya.hashim@hotmail.com",
+position:"Audiologist", admin:true, password:"aliya12",
+password_confirmation:"aliya12")
+
+Medic.create(first_name:"Ola", last_name:"Daher", organization_id:1,
+email:"ola@gmail.com",
+position:"Allergist", admin:false, password:"ola123",
+password_confirmation:"ola123")
+
+Medic.create(first_name:"Daniel", last_name:"Chweiky", organization_id:1,
+email:"dan@gmail.com",
+position:"Andrologist", admin:false, password:"dan123",
+password_confirmation:"dan123")
+
+Medic.create(first_name:"Srin", last_name:"Chakrav", organization_id:2,
+email:"schakrav@cmu.edu",
+position:"Anesthesiologist", admin:true, password:"schakrav19",
+password_confirmation:"schakrav19")
+
+Medic.create(first_name:"Aboudue", last_name:"Whodunnit", organization_id:2,
+email:"abd@gmail.com",
+position:"Cardiologist", admin:false, password:"abd123",
+password_confirmation:"abd123")
+
+Medic.create(first_name:"Homolika", last_name:"Komolika", organization_id:2,
+email:"kasauti@gmail.com",
+position:"Dentist", admin:false, password:"kasauti123",
+password_confirmation:"kasauti123")
+
+Organization.create(name:"", region:"West Bay", phone:"97466090820")
+
+Organization.create(name:"Hamad Medical Corporation", region:"Al Bidda", phone:"74445632157")
+
+Organization.create(name:"Al Ahli Hospital", region:"Al Dafna", phone:"97433190829")
+
+Organization.create(name:"Doha Clinic", region:"Al Hilal", phone:"97466094441")
+
+
 
 
 
