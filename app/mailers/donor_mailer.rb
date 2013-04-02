7bd8ea3613c6b74e8350ca9e711b511cc0a80bda
Aliya-cmuq <ahashim@qatar.cmu.edu>
2013-04-02 17:22:27 +0300

    Email Done

--- app/mailers/donor_mailer.rb
+++ app/mailers/donor_mailer.rb
@@ -1,10 +1,12 @@
 class DonorMailer < ActionMailer::Base
   default from: "lifespan.donation@gmail.com"
-  def new_donor_request(donor_par, medic, org)
+  def new_donor_request(donor_par, medic, org, blood)
     @donorEmail = donor_par.map{|o| o.email}
     @medic_name = medic.proper_name
     @medic_org = org.name
     @medic_region = org.region
+    @blood_needed = blood
+    
     # @bloodtype = donor_par.map{|o| o.proper_name}
         mail(:to => "lifespan.donation@gmail.com", :bcc => @donorEmail,
         :subject => "Blood Donation Request")
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lifespan::Application.initialize!
+
+
+Twitter.configure do |config|
+  config.consumer_key = "BQNO5N3UYzWRXeLyRdwg"
+  config.consumer_secret = "wXjzMGkhzKHOT5eUV6CzcXKuXxM6ZGktrCBBOKaG4"
+  config.oauth_token = "1271919566-df5d29vJ8BUUZD1MIKJEgKRyb8mMeieCZ1gEYLX"
+  config.oauth_token_secret = "GpSLAj1GcHFR4SAraXvUTRvuVvUzEO8qbwftrBWHUTA"
+end
