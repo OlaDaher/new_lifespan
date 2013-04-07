a26021aa6a2bc6aaad144551872c8cc1fc2a9e03
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-04-07 23:50:37 +0300

    deleting self account not working

--- app/models/medic_ability.rb
+++ app/models/medic_ability.rb
@@ -2,23 +2,27 @@ class MedicAbility
   include CanCan::Ability
 
     def initialize(medic)
+        cannot :manage, Medic 
         can [:show, :update, :destroy], Medic, :id => medic.id
         cannot :manage, Organization
         can :read, Organization
-        cannot [:create, :update, :destroy], Donor
+        cannot :manage, Donor
         can :read, Donor
         can :send_request, Donor
+        can :send_one_blood_type, Donor
         
         if medic.admin == true
             can :manage, Medic
-           
+            can :manage, Organization
+            cannot :manage, Donor
+            can :read, Donor
+            can :send_request, Donor
+            can :send_one_blood_type, Donor
             #cannot :create, Medic
             #can(:create, Medic) do |organization|
             #    permitted = organization.id === medic.organization_id
             #    permitted
             #end
-         else 
-            cannot :manage, Medic    
         end
     end        
 end    
