85c5cdaaac6c62cf2e6d85dda130c8fe8e85a48a
Aliya-cmuq <ahashim@qatar.cmu.edu>
2013-04-09 16:43:29 +0300

    No idea

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -133,9 +133,10 @@ class DonorsController < ApplicationController
     DonorMailer.new_donor_request(@donors, @medic, @org, @blood).deliver
 
     @twitter = "#{@blood} is needed at #{@org.name} in #{@org.region}, telephone: +#{@org.phone}. Sent: #{@time}. SHARE IT AND SAVE SOMEONE'S LIFE"
-
+    @requests << @twitter
     Twitter.update(@twitter)
-    initialize(@blood)
+
+    # initialize(@blood)
     redirect_to root_url
     
   end
