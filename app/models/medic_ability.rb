dad36358ac49827f5f6bfd3410a12e025c42fc24
Srinjoy Chakravarty <chaxz93@gmail.com>
2013-04-19 01:09:46 +0300

    latest changes

--- app/models/medic_ability.rb
+++ app/models/medic_ability.rb
@@ -13,12 +13,13 @@ class MedicAbility
         
         if medic.admin == true
             cannot :manage, Medic
-            can :manage, Organization
+            cannot :manage, Organization
             cannot :manage, Donor
             can :read, Donor
             can :send_request, Donor
             can :send_one_blood_type, Donor
-            can :manage, Medic, :organization_id => medic.organization_id
+            can [:create, :read, :destroy], Medic, :organization_id => medic.organization_id
+            can [:read, :update], Organization, :id => medic.organization_id
      
             #can(:create, Medic) do |organization|
             #    permitted = organization.id === medic.organization_id
